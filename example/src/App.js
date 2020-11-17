36369127788df09662b103c6501654035ad76455
Nguyễn Thị Ngọc Huyền <ntnh5600@gmail.com>
2020-11-18 00:45:48 +0700

    Enter

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import { ExampleComponent } from 'demolock'
 import 'demolock/dist/index.css'
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { date: new Date() }
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+  //lifecycle method
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  render() {
+    return (
+      <div className='time-open'>
+        <ExampleComponent text='Demo Clock 😄' />
+        Thời gian mở web: {this.state.date.toLocaleTimeString()}
+        <i> vào</i> <b>{this.state.date.toLocaleDateString()}</b>
+      </div>
+    )
+  }
 }
 
 export default App
